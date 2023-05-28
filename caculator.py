5af894ec3b2e2b22bf0850efaf1b53ed9cefca15
Krishna Chaitanya Rakonda <krishchai007@gmail.com>
2023-05-28 17:16:21 -0600

    added a division function

--- caculator.py
+++ caculator.py
@@ -12,6 +12,13 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+# Function to divide two numbers
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        print("Error: Division by zero!")
+
 # Main program
 print("Welcome to the Calculator Program!")
 
@@ -20,11 +27,12 @@ while True:
     print("1. Addition")
     print("2. Subtraction")
     print("3. Multiplication")
-    print("4. Exit")
+    print("4. Division")
+    print("5. Exit")
 
-    choice = int(input("Enter your choice (1-4): "))
+    choice = int(input("Enter your choice (1-5): "))
 
-    if choice == 4:
+    if choice == 5:
         print("Exiting the program...")
         break
 
@@ -40,7 +48,10 @@ while True:
     elif choice == 3:
         result = multiply(num1, num2)
         print("Result: ", result)
+    elif choice == 4:
+        result = divide(num1, num2)
         if result:
             print("Result: ", result)
 
     print("--------------------")
+
